da445f73372804bbcd5d4c31013b65c4d3026837
jonatanalvelid <35332627+jonatanalvelid@users.noreply.github.com>
2020-05-19 11:06:27 +0200

    updates

--- findmaxima.py
+++ findmaxima.py
@@ -9,7 +9,7 @@ def findmaxima(img, min_dist=3, thresh_abs=3):
     # find map with all pixels that are local peaks with min_dist and abs._thresh
     is_peak = peak_local_max(img, min_distance=min_dist, threshold_abs=thresh_abs, num_peaks_per_label=1, indices=False)
     # label all peaks with a connectivity of structure (8-connectivity as of now)
-    labels_peaks = ndi.measurements.label(is_peak, structure=np.ones((3,3)))[0]
+    labels_peaks = ndi.measurements.label(is_peak, structure=np.ones((3, 3)))[0]
     # merge coordinates close to each other to a common center of mass (8-connectivity)
     coords_merged = ndi.measurements.center_of_mass(is_peak, labels_peaks, range(1, np.max(labels_peaks)+1))
     coords_merged = np.array(coords_merged)
--- mainScript.py
+++ mainScript.py
@@ -22,30 +22,30 @@ filepath = os.path.join(dirpath, 'C-Cell010.tif')
 
 # read image and preprocess
 imgraw = cv.imread(filepath)
-imgraw = imgraw[400:600, 300:500, 1]
+imgraw = imgraw[300:600, 300:600, 1]
 #imgraw = imgraw[:,:,1]
-img = ndi.gaussian_filter(imgraw, 0.8)
+img = ndi.gaussian_filter(imgraw, 0.7)
 
 # img parameters
 pxs_nm = 30  # pixel size in nm
 
 # get the peak coordinates
-coords_peaks = findmaxima(img, thresh_abs=7, min_dist=2)
+coords_peaks = findmaxima(img, thresh_abs=5, min_dist=2)
 
 # get density map of peaks
-bandwidth_nm = 120
+bandwidth_nm = 300
 X, Y, Z = densitymap(coords_peaks, img, bandwdt=bandwidth_nm/pxs_nm)
 
 # plotting
 # plotting of raw image and peak coordinates
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].imshow(imgraw, interpolation='none', cmap=plt.cm.hot)
-axs[0, 1].imshow(img, interpolation='none', cmap=plt.cm.hot)
-axs[0, 1].plot(coords_peaks[:, 1], coords_peaks[:, 0], 'g.')
+fig, axs = plt.subplots(1, 2)
+# axs[0, 0].imshow(imgraw, interpolation='none', cmap=plt.cm.hot)
+axs[0].imshow(img, interpolation='none', cmap=plt.cm.hot)
+axs[0].plot(coords_peaks[:, 1], coords_peaks[:, 0], 'g.')
 # plot contours of the density estimate
-levels = np.linspace(Z.max()/3, Z.max(), 25)
-axs[1, 0].contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
-axs[1, 0].set_aspect('equal', 'box')
+levels = np.linspace(0, Z.max(), 25)
+axs[1].contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
+axs[1].set_aspect('equal', 'box')
 
 fig.tight_layout()
 plt.show()
